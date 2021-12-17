95b2158c0e32d65816582d11b660077ae3ed82be
yammybajwa <92578770+yammybajwa@users.noreply.github.com>
2021-12-17 06:41:38 -0800

    Add array and working on Button

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const but =document.getElementById("genrate");
 const result =document.getElementById("res");
+let array=["lorem ipsum","lorem "," ipsum","john","lorem ipsum genrate"]
+but.onclick= function(){
+    const get = Math.floor(Math.random()*array.length) ;
+    console.log(isNaN(array))
+   if(isNaN(array) || array<0||array>4){
+       result.innerHTML=array[get];
+   }
+}
+    
